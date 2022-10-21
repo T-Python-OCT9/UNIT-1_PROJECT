fbdb3360e3223d4e21eae7c0f42eac9e5cd070b2
Alaa Alfraih <Alfraih.alaa@gmail.com>
2022-10-22 00:12:45 +0300

    two Oct22

--- main.py
+++ main.py
@@ -16,10 +16,17 @@ coffeeDictionary={"coffee1":15,
 item_coffee=coffeeDictionary.items()
 coffee.coffee_menu(item_coffee)
 
-start=input("\nIf you want drink coffee enter \"*\"")
+start=input("\nIf you want drink coffee enter \"*\"\n")
 while(start=="*"):
+    print("\nTo chose coffee1 enter \"1\"\nTo chose coffee2 enter \"2\"\nTo chose coffee3 enter \"3\"\nTo chose coffee4 enter \"4\"\nTo chose coffee5 enter \"5\"\nTo chose coffee6 enter \"6\"")
     chose_coffee=input("Chose coffee you want plese:")
-    break
+    complete_order=input("if you want to complete your order enter \"y\",if you want to cancel your order enter\"n\":\n")
+    if complete_order=="y"or complete_order=="Y":
+        print("price")
+        break
+    elif complete_order=="n" or complete_order=="N":
+        break
+    
     #pass
     #coffee menu
 
