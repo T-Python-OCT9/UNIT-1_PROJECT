3a9e5ffed6b4c49540cd4bd4d0dc45e3a14223b5
Athari_Alharbi <AthariAlharbiA@hotmail.com>
2022-10-22 04:11:00 +0300

    project1+

--- BooksName.py
+++ BooksName.py
@@ -1,6 +1,8 @@
+import random
 
 class BooksName:
     Books = ["Do not give up","History remains open","Les Misérables","Night train to Lisbon"]
+    
 
     book = input("Enter the name of the book you want: ")
 
@@ -11,21 +13,40 @@ class BooksName:
         elif not(book in Books):
             print("sorry this book is not in the librari.")
         break 
-    #باقي انا اقترح عليه كتاب عشوائي
 
-    def RandomBook():
-        print("Do you want to suggest a random book to you? ")
-    
+     
 
     writers = {"Turki Alhamad" : "History remains open", "Victor" : "Les Misérables","Pascal" : "Night train to Lisbon","Dr. Ayed Al-Qarni" : "Do not give up"}
     
-    authors = input("Enter the author's name to view his existing books: ")
+    authors = input("Enter the author name to view his existing books: ")
 
     for j in writers.values():
 
         if authors in writers:
-           print(writers[authors]) 
+           print(writers[authors])
+        else:
+           print("sorry")    
         break
+
+
+ #اقتراح كتاب عشوائي للمستخدم
+    def RandomBook(Books):
+
+        randomly = input("Do you want to suggest a random book to you? yes or no ")
+
+        if randomly == 'yes':
+            return random.choice(Books)
+            
+        if randomly == 'no' :
+           print("OK,Thank you") 
+
+    book_random = print("We have suggested book",RandomBook(Books),"for you")  
+
+    #lambda          
+    
+
+
+     
         
 
 
