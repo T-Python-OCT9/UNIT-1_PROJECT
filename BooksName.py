b281c187e446f734366a57bbee9d8af736ed4538
Athari_Alharbi <AthariAlharbiA@hotmail.com>
2022-10-24 01:46:56 +0300

    project1submit

--- BooksName.py
+++ BooksName.py
@@ -1,32 +1,39 @@
+from numbers import Number
 import random
+from unicodedata import numeric
 import BooksPrice
-class BooksName:
-    Books = ["Do not give up","History remains open","Les Misérables","Night train to Lisbon"]
 
+
+class BooksName:
+    #البحث عن كتاب في المكتبة
+    Books = ["Do not give up","History remains open","Les Misérables","Night train to Lisbon"]
     book = input("Enter the name of the book you want: ")
 
     for i in Books:
         
         if book in Books:
-           print("yes this book is in the library.",book)           
+           print("yes this book is in the library. ")           
         elif not(book in Books):
-            print("sorry this book is not in the librari.")
+           print("sorry this book is not in the librari. ")
         break 
 
-    
+
+
+
+    #عرض كتب لكاتب معين
     '''writers =  [
             {'Turki Alhamad': [ 'History remains open','East valley','Memory wounds']},
             {"Victor":[ "Les Misérables"]},
             {"Pascal": [ "Night train to Lisbon"]},
-            {"Dr. Ayed Al-Qarni": ["Do not give up"]}
+            {"Dr.Ayed Al-Qarni": ["Do not give up"]}
             ]'''
 
 
     writers = {'Turki Alhamad': 'History remains open',"Victor":"Les Misérables","Pascal":"Night train to Lisbon","Dr. Ayed Al-Qarni": "Do not give up"}
     writers["Turki Alhamad"] = ['East valley','Memory wounds','History remains open']
-     #print(writers["Turki Alhamad"])
+    #print(writers["Turki Alhamad"])
 
-     #ممكن اسوي الاكسبشن هنا لو دخل اليوزر اسم الكاتب غلط
+     
     authors = input("Enter the author name to view his existing books: ")
 
     for j in writers.values():
@@ -34,31 +41,44 @@ class BooksName:
         if authors in writers:
            print(writers[authors])
         else:
-           print("sorry")    
+           print("sorry,Ther are no books for this author in the library. ")    
         break
+
+
 
 
  #اقتراح كتاب عشوائي للمستخدم
     def RandomBook(Books):
-
-        randomly = input("Do you want to suggest a random book to you? yes or no ")
+     
+        randomly = input("Do you want to suggest a random book to you? yes or no? ")
 
         if randomly == 'yes':
-            return random.choice(Books)
+           return f'We have suggested book {random.choice(Books)} for you'
             
-        if randomly == 'no' :
-           #print("OK,Thank you")
-            return 
+        elif randomly == 'no' :
+           print("OK,Thank you ")
 
-    book_random = print("We have suggested book",RandomBook(Books),"for you")  
+        else:
+            if randomly.isnumeric() and randomly.isnumeric():
+                raise Exception("pleas enter yes or no. ") 
+                 
+                         
+    book_random = print(RandomBook(Books))  
+
+
+
 
     #lambda
     def FunctionPrice1(): 
-       p = int(print("to view books prices last more than 50 you shoud write 1 pleas."))
-       #if(p == 1):
-           #FunctionPrice2()
 
-    
+       p = input("to view books prices last more than 50 you shoud write 1 pleas. ")
+       int(p)
+
+       if(p == 1):
+           BooksPrice.FunctionPrice2()
+
+
+    FunctionPrice1()
 
 
      
--- BooksPrice.py
+++ BooksPrice.py
@@ -1,21 +1,26 @@
-#import BooksName 
+import BooksName
+
 class BooksPrice:
+
+    print("These books are priced under 50: ")
+    
     def FunctionPrice2():
         price = {35 : "History remains open", 42 : "Les Misérables",78 : "Night train to Lisbon",15 : "Do not give up"}
        
         for j in price:
+
            price_list = price.keys()
         
            price_list_new =  list(filter(lambda price_list : price_list < 50 , price))
-           print(price_list_new)
-
+           #print(price_list_new)
            break
 
         for i in price_list_new:
             
-             if i in price.keys():
-                print(price.values())
-                break
+            if i in price.keys():
+               print(price[i]) #عرض الكتب اللي اقل من 50
+        
+                   
+    FunctionPrice2()  
 
-
-    FunctionPrice2()        
+        
