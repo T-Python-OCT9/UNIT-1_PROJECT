85aa3d4ebfcc493d46cfcfdef2a034281a92f54b
Alaa Alfraih <Alfraih.alaa@gmail.com>
2022-10-23 20:04:40 +0300

    four

--- CoffeeType.py
+++ CoffeeType.py
@@ -2,42 +2,25 @@
 #for lis menu to user
 from nis import match
 
-
 def coffee_menu(item):
     for key,valu in item:
        print(f"{key}\t\t\t{valu}")
 
-def coffee_choise(coffee_chose):
-    if coffee_chose=="1":
-        print("1")
-    elif coffee_chose=="2":
-        print("2")
-    elif coffee_chose=="3":
-        print("3")
-    elif coffee_chose=="4":
-        print("4")
-    elif coffee_chose=="5":
-        print("5")
-    elif coffee_chose=="6":
-        print("6")
     
-
-def itemCoffee(choice):
-    coffeeType={"1":13, "2":11, "3":12, "4":13, "5":13, "6":10}
-    number=0
-    for key ,value in coffeeType.items():
-       if key == choice:
-           number=1
-           return choice
-       else:
-            continue
-    if number==0:
-        print("error")
-
-
-def coffeTy(number):
+def coffeePrice(number):
     count=0
-    coffeeType={"1":13, "2":11, "3":12, "4":13, "5":13, "6":10}
+    coffeeType={"1":15, "2":13, "3":12, "4":14, "5":16, "6":15}
+    for n in coffeeType:
+        if n==number:
+            item=coffeeType.get(n)
+            count=1
+    if count==0:
+        raise ValueError("you must chose coffee ")
+    return item
+
+def coffeeName(number):
+    count=0
+    coffeeType={"1":"coffee1", "2":"coffee2", "3":"coffee3", "4":"coffee4", "5":"coffee5", "6":"coffee6"}
     for n in coffeeType:
         if n==number:
             item=coffeeType.get(n)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import CoffeeType as coffee
+import Classes
 
 
 #main class
@@ -16,21 +17,48 @@ coffeeDictionary={"coffee1":15,
 item_coffee=coffeeDictionary.items()
 coffee.coffee_menu(item_coffee)
 
+
+#lambda 
+tax_calculation=lambda price_lambda:price_lambda+(price_lambda*0.15)
+
 #To start program
 start=input("\nIf you want drink coffee enter \"*\"\n")
 while(start=="*"):
     print("\nTo chose coffee1 enter \"1\"\nTo chose coffee2 enter \"2\"\nTo chose coffee3 enter \"3\"\nTo chose coffee4 enter \"4\"\nTo chose coffee5 enter \"5\"\nTo chose coffee6 enter \"6\"")
     choice=input("Chose coffee you want plese:")
     try:
-        price=coffee.coffeTy(choice)
+        price=coffee.coffeePrice(choice)
+        coffee_name=coffee.coffeeName(choice)
         print(f"price is : {price}")
     except ValueError as error:
         print(error)
         break
     complete_order=input("if you want to complete your order enter \"y\",if you want to cancel your order enter\"n\":\n")
     if complete_order=="y"or complete_order=="Y":
-        print("price")
-        break
+        print("How do you want to \'?\'\n*Note, If you buy from card you will have discount 20%")
+        buy=input("enter \"1\" to chose card or \"2\" to cash: \n")
+        if buy=="1":
+            card_buy=Classes.Card(price)
+            discount_price=card_buy.printInfo()
+            print(f"Your order is {coffee_name} and the price after discount is {discount_price}, The price after calculator tax is {tax_calculation(discount_price)}")
+            complete_with_card= input("enter C to buy, if you want to cancel enter n:\n")
+            if complete_with_card=="c" or complete_with_card=="C":
+                print(f"your coffee {coffee_name} is ready, take it pleas ")
+                start=input("do you want order another coffee ? if you want enter \"*\" if you do not enter no")
+            elif complete_with_card=="n" or complete_with_card=="N":
+                pass
+        elif buy=="2":
+            user_m=input("enter your mony plese: \n")
+            try:
+                cash_buy=Classes.Cash(price,float(user_m))
+                mouny_p:float=cash_buy.printInfo()
+            except TypeError as error:
+                print(error)
+                break
+            print(f"Your order is {coffee_name} and the mony after discount price is {mouny_p}, The price after calculator tax is {tax_calculation(price)}\nTake \'?\'")
+            print(f"your coffee {coffee_name} is ready, take it pleas ")
+            start=input("do you want order another coffee ? if you want enter \"*\" if you do not enter no")
+
     elif complete_order=="n" or complete_order=="N":
         break
 
