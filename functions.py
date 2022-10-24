1bf762221ed243789f9d9b23286b7cba52b0ea2c
Abdullah <a.fahad9@outlook.com>
2022-10-24 10:29:07 +0300

    added some codes to make the CLI cleaner, also added lambda function to calculate the sum

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import random
+from functools import reduce
 '''
 
 Made this seperate class to make the main looks more clean :D 
@@ -9,7 +10,7 @@ class PersonalWallet():
     
     def main():
         print("*--- Hello and Welcome To Your Personal Wallet ! ---*\n")
-        name_list, acc_num_list, acc_blc_list = PersonalWallet.read_file()
+        name_list, acc_num_list, acc_balance_list = PersonalWallet.read_file()
         option = 0
         while option != 6:
 
@@ -40,29 +41,29 @@ class PersonalWallet():
 
     # if statement for all of the options
             if option == 1:
-                name_list, acc_num_list, acc_blc_list = PersonalWallet.open_acc(name_list, acc_num_list, acc_blc_list)
+                name_list, acc_num_list, acc_balance_list = PersonalWallet.open_acc(name_list, acc_num_list, acc_balance_list)
 
             elif option == 2:
-                name_list, acc_num_list, acc_blc_list = PersonalWallet.close_account(name_list, acc_num_list, acc_blc_list)
+                name_list, acc_num_list, acc_balance_list = PersonalWallet.close_account(name_list, acc_num_list, acc_balance_list)
 
             elif option == 3:
-                name_list, acc_num_list, acc_blc_list = PersonalWallet.withdraw(name_list, acc_num_list, acc_blc_list)
+                name_list, acc_num_list, acc_balance_list = PersonalWallet.withdraw(name_list, acc_num_list, acc_balance_list)
 
             elif option == 4:
-                name_list, acc_num_list, acc_blc_list = PersonalWallet.add_money(name_list, acc_num_list, acc_blc_list)
+                name_list, acc_num_list, acc_balance_list = PersonalWallet.add_money(name_list, acc_num_list, acc_balance_list)
 
             elif option == 5:
-                PersonalWallet.summary(name_list, acc_num_list, acc_blc_list)
+                PersonalWallet.summary(name_list, acc_num_list, acc_balance_list)
 
             elif option == 6:
-                PersonalWallet.exit(name_list, acc_num_list, acc_blc_list)
+                PersonalWallet.exit(name_list, acc_num_list, acc_balance_list)
 
 
     # Functions for all of the switch statement
     def read_file():
         name_list = []
         acc_num_list = []
-        acc_blc_list = []
+        acc_balance_list = []
 
     # reading the text file
         f = open("wallets_data.txt", "r", encoding="utf-8")
@@ -70,13 +71,13 @@ class PersonalWallet():
         for line in lines:
             information = line.split()
             acc_num_list.append(information[0])
-            acc_blc_list.append(float(information[1]))
+            acc_balance_list.append(float(information[1]))
             name_list.append(information[2])
-        return name_list, acc_num_list, acc_blc_list
+        return name_list, acc_num_list, acc_balance_list
 
 
     # Function for opening an account
-    def open_acc(name_list, acc_num_list, acc_blc_list):
+    def open_acc(name_list, acc_num_list, acc_balance_list):
         name = input("Please enter your name: ")
         print("Your name: ", name)
         name_list.append(name)
@@ -88,15 +89,15 @@ class PersonalWallet():
         acc_num_list.append(number)
 
         initial_balance = float(input(f"Please Enter the Initial balance for {name}: "))
-        acc_blc_list.append(initial_balance)
+        acc_balance_list.append(initial_balance)
 
         print(f"-- An account has been opened ! \n- Account name: {name} \n- initial balance of {name} is : {initial_balance} \n- Account number is: {number}")
-
-        return name_list, acc_num_list, acc_blc_list
+        print("*"*55)
+        return name_list, acc_num_list, acc_balance_list
 
 
     # Function for closing an account
-    def close_account(name_list, acc_num_list, acc_blc_list):
+    def close_account(name_list, acc_num_list, acc_balance_list):
         account_number = input("\nEnter your account number:\n")
         index = 0
         found = False
@@ -106,19 +107,21 @@ class PersonalWallet():
                 break
             index = index + 1
         if found:
-            print(f"\n --- Closing the account {name_list[index]} --- \n - Account number was: {acc_num_list[index]} \n - Total balance of account was: {acc_blc_list[index]} \n -- Sorry to see you go *{name_list[index]}* --\n")
+            print(f"\n --- Closing the account: {name_list[index]} --- \n - Account number was: {acc_num_list[index]} \n - Total balance of account was: ${acc_balance_list[index]:,} \n -- Sorry to see you go *{name_list[index]}* --\n")
+            print("*"*55)
+
             # deletes the 3 lists from their index position.
             del acc_num_list[index]
-            del acc_blc_list[index]
+            del acc_balance_list[index]
             del name_list[index]
 
         else:
             print("\nError -- No account exists under the number you entered \n")
-        return name_list, acc_num_list, acc_blc_list
+        return name_list, acc_num_list, acc_balance_list
 
 
     # Function for Withdrawing money from an account
-    def withdraw(name_list, acc_num_list, acc_blc_list):
+    def withdraw(name_list, acc_num_list, acc_balance_list):
         account_number = input("\nEnter your account number:\n")
         index = 0
         found = False
@@ -136,30 +139,31 @@ class PersonalWallet():
                     if withdraw_amount < 0:  # if not a positive int print message and ask for input again
                         print("ERROR -- input must be a positive integer, try again")
                         break
-                    amount = acc_blc_list[index] - withdraw_amount
+                    amount = acc_balance_list[index] - withdraw_amount
                     # if the amount is greater than or = 0 it will take the amount away from the balance list.
                     if amount > 0:
-                        acc_blc_list[index] = acc_blc_list[index] - withdraw_amount
+                        acc_balance_list[index] = acc_balance_list[index] - withdraw_amount
                         print(f"An amount of ${(withdraw_amount)} is removed from your account {account_number}")
-                        print(f"Your current balance is $ {acc_blc_list[index]:,} ")
+                        print(f"Your current balance is $ {acc_balance_list[index]:,} ")
                         print("")
                         problem = False
                     else:
-                        print("Unfortunately you don't have a sufficient funds",
-                            name_list[index])
-                        print("Your balance after your current transaction is ", "$",
-                            format(acc_blc_list[index], ".2f"), sep="")
+                        print(f"Unfortunately you don't have a sufficient funds{name_list[index]:,}")
+                        print(f"Your balance after your current transaction is $ {acc_balance_list[index]}:,")
                         print("")
+                        print("*"*55)
                         break
                 finally:
-                    return name_list, acc_num_list, acc_blc_list
+                    return name_list, acc_num_list, acc_balance_list
+                    
         else:
             print("\n Sorry that account number does not exist \n")
-            return name_list, acc_num_list, acc_blc_list
+            print("*"*55)
+            return  name_list, acc_num_list, acc_balance_list
 
 
     # Function for depositing an amount to an account
-    def add_money(name_list, acc_num_list, acc_blc_list):
+    def add_money(name_list, acc_num_list, acc_balance_list):
         account_number = input("\nEnter your account number:\n")
         index = 0
         found = False
@@ -177,43 +181,47 @@ class PersonalWallet():
                     if deposit_amount < 0:  # if not a positive int print message and ask for input again
                         print("ERROR -- input must be a positive integer, try again")
                         break
-                    amount = acc_blc_list[index] + deposit_amount
+                    amount = acc_balance_list[index] + deposit_amount
                     # if the amount is greater than or = 0 it will added to the balance list.
                     if amount > 0:
-                        acc_blc_list[index] = acc_blc_list[index] + deposit_amount
+                        acc_balance_list[index] = acc_balance_list[index] + deposit_amount
                         print(f"An amount of $ {deposit_amount} is added to your account {account_number}")
-                        print(f"Your current balance is $ {acc_blc_list[index]:,} ")
+                        print(f"Your current balance is $ {acc_balance_list[index]:,} ")
                         print("")
                         problem = False
                     else:
                         print(f"Error -- Please enter a positive number {name_list[index]}")
-                        print(f"Your balance after your current transaction is $ {acc_blc_list[index]}")
+                        print(f"Your balance after your current transaction is $ {acc_balance_list[index]}")
                         print("")
                         break
                 finally:
-                    return name_list, acc_num_list, acc_blc_list
+                    return  name_list, acc_num_list, acc_balance_list
         else:
             print("\n Sorry that account number does not exist \n")
-            return name_list, acc_num_list, acc_blc_list
+            print("*"*55)
+            return name_list, acc_num_list, acc_balance_list
 
 
     # Function to display a report for all the accounts details
-    def summary(name_list, acc_num_list, acc_blc_list):
+    def summary(name_list, acc_num_list, acc_balance_list):
         if len(name_list) > 0:
             for i in range(len(acc_num_list)):
-                print(f"\nClient {name_list[i]} who's account number is {acc_num_list[i]} has $ {acc_blc_list[i]: ,}")
-                total = sum(acc_blc_list)
-            print(f"\nTotal amount of deposit in the system is: {total: ,}")
-            largest_deposit = max(acc_blc_list)
+                print(f"\nClient {name_list[i]} who's account number is {acc_num_list[i]} has $ {acc_balance_list[i]: ,} \n")
+                #using lambda to get the total deposited 
+                total = reduce(lambda total_depo, current: total_depo+current, acc_balance_list)
+            print(f"\nTotal amount of deposit in the system is: ${total: ,}")
+            largest_deposit = max(acc_balance_list)
             print(f"\nThe largest amount of deposit in the system is:  ${largest_deposit: ,}\n ")
+            print("*"*55)
         else:
-            print("There is no account data !")
+            print("There is no account data !")   
 
-    def exit(name_list, acc_num_list, acc_blc_list):
+    def exit(name_list, acc_num_list, acc_balance_list):
         print("Thank You For Using The Personal Wallet program !")
         quit_file = open("wallets_data.txt", "w", encoding="utf-8")
         for i in range(len(acc_num_list)):
-            save = acc_num_list[i] + " " + str(acc_blc_list[i]) + " " + name_list[i] + "\n"
+            save = acc_num_list[i] + " " + str(acc_balance_list[i]) + " " + name_list[i] + "\n"
             quit_file.write(save)
         quit_file.close()
+        print("*"*55)
 
