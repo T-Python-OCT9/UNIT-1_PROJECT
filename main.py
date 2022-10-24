75f8e663b23eaa416a08af7d9c949b8a5d675cec
Nourah Alqhtani <nourah0qq@gmail.com>
2022-10-24 16:32:24 +0300

    Add Error Handling

--- main.py
+++ main.py
@@ -19,34 +19,39 @@ while True:
         print(list)
         file.close()
         print()
+        try:
+            Selection = int(input("Make your selection: "))
+        except ValueError :
+            print("You did not make a valid selection.")
 
-        Selection = input("Make your selection: ")
-        if Selection == "1":
+        
+        if Selection == 1:
             print()
             print("-------View Coffee List-------")
             User1.View_Coffee_list()
             print("-------------------------------\n")
-        elif Selection == "2":
+        elif Selection == 2:
             print()
             print("-----------Add Item-----------")
             User1.View_Coffee_list()
             print()
             User1.Add_Item()
-        elif Selection == "3":
+        elif Selection == 3:
             print()
             print("----------Remove Item----------")
             User1.Remove_Item()
-        elif Selection == "4":
+        elif Selection == 4:
             print()
             print("------------Receipt------------")
             User1.Receipt()
-        elif Selection == "5":
+        elif Selection == 5:
             User1.Clear_List()
-        elif Selection == "6":
+        elif Selection == 6:
             print("\nHope to see you back soon!\n")
             break
         else:
-            print("You did not make a valid selection.")
+            print("Enter the valid number.")
+        
     More_User = input("Do you have anther user yes or no? ")
     if More_User.lower() == "yes":
         pass
